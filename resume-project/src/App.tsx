9a86e885daff03fdc860d7292fdbdc2f4948c0c9
Iso-latte <trujillo.joaquin101@gmail.com>
2025-01-09 17:31:01 -0700

    added dropdown removed navbar -left

--- resume-project/src/App.tsx
+++ resume-project/src/App.tsx
@@ -3,6 +3,8 @@ import Education from "./componets/Education";
 import TechSkills from "./componets/TechSkills";
 import Experience from "./componets/Experience";
 import About from "./componets/About";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return <div>
