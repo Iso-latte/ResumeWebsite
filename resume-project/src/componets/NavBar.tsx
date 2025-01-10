df16c2df2f8cbfa81c47b255302909ea8d9accb0
Iso-latte <trujillo.joaquin101@gmail.com>
2025-01-10 13:53:50 -0700

    added correct links to socials

--- resume-project/src/componets/NavBar.tsx
+++ resume-project/src/componets/NavBar.tsx
@@ -36,13 +36,13 @@ function Navbar() {
         {/* Right section: Social Media links */}
         <div className="navbar-right">
           <div className="backdrop">
-            <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.youtube.com/@isolatte3739" target="_blank" rel="noopener noreferrer">
               <img src="./yt_icon_rgb.png" alt="YouTube" width="28" height="25" />
             </a>
-            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Iso-latte" target="_blank" rel="noopener noreferrer">
               <img src="./github-mark.png" alt="GitHub" width="25" height="25" />
             </a>
-            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/joaquin-trujillo-6476641a1/" target="_blank" rel="noopener noreferrer">
               <img src="./LI-In-Bug.png" alt="LinkedIn" width="28" height="25" />
             </a>
           </div>
@@ -59,7 +59,7 @@ function Navbar() {
               <img src="./Home.png" alt="House_photo" />
               Home
             </Link>
-            {/* 
+            {/*
             <Link to="/projects/" className='nav-link'>
               <img src="./Projects.png" alt="Pile_of_papers" />
               Projects
@@ -75,7 +75,6 @@ function Navbar() {
               Connect
             </Link>  
             */}
-          
           </div>
         </div>
       </div>
