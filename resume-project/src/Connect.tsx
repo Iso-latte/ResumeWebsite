010868a10183cdce0633b58a9e9c12013656a1e0
isolate <trujillo.joaquin101@gmail.com>
2026-05-18 07:59:32 -0600

    test

--- resume-project/src/Connect.tsx
+++ resume-project/src/Connect.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import NavBar from "./componets/NavBar";
 import ConnectForm from "./componets/ConnectForm";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-function App() {
+function App(): React.ReactElement {
   return <div>
           <NavBar/>
           <ConnectForm/>
